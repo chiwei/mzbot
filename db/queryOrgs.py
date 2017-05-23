911dd45eb1c95dbb0d3c6512a7e1dede03281f07
chiwei <chwx86@gmail.com>
2017-05-23 18:01:20 +0800

    fixs

--- db/queryOrgs.py
+++ db/queryOrgs.py
@@ -9,7 +9,7 @@ def queryOrgByCode(orgCode):
     c.execute('select areacode,areaname,orgcode,orgname,usci,tkmc from orgstd where orgcode=?',[orgCode])
     for row in c:
         if row[0]!=None:
-            resultStr ='单位代码：'+orgCode+'\n'+'查询结果：\n'+'单位名称：'+row[3]+'   '+'区划代码：'+row[0]+'    '+'区划名称：'+row[1]+'   '+'所在台卡：'+row[5]
+            resultStr ='单位代码：'+orgCode+'\n单位名称：'+row[3]+'   '+'区划代码：'+row[0]+'    '+'区划名称：'+row[1]+'   '+'所在台卡：'+row[5]
         print(resultStr)
     c.close()
     return resultStr
--- db/queryRegions.py
+++ db/queryRegions.py
@@ -7,7 +7,6 @@ def queryRegionByCode(regioncode):
     conn = sqlite3.connect('data/regionlib.db')
     c = conn.cursor()
     c.execute('select year,region_code,region_name from regionlib where region_code=? order by year desc',[regioncode])
-    print(c.rowcount)
     for row in c:
         if row[0]!= None:
             resultStr+='年份:'+row[0]+'    '+'区划代码：'+row[1]+'    '+'区划名称：'+row[2]+'\n'
